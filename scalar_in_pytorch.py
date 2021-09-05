622a273604431d3dfbc771d9458303be8f482324
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-09-06 01:58:58 +0600

    Create scalar_in_pytorch.py

---
+++ scalar_in_pytorch.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  6 01:44:45 2021
+
+@author: Mahfuz_Shazol
+"""
+
+import torch
+
+x_pt=torch.tensor(25)
+print(x_pt)
+print(type(x_pt))
+
+print(x_pt.shape)
