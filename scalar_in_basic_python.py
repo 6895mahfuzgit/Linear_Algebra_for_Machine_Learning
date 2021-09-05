8e4a6d6e4753a58512f990f90612512067ff54e3
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-09-06 01:44:20 +0600

    Create scalar_in_basic_python.py

---
+++ scalar_in_basic_python.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  6 01:34:50 2021
+
+@author: Mahfuz_Shazol
+"""
+
+x=25
+print(x)
+print(type(x))
+
+y=3
+
+py_sum=x+y
+
+print(py_sum)
+print(type(py_sum))
+
+x_float=25.0
+
+float_sum=x_float+y
+print(float_sum)
+print(type(float_sum))
