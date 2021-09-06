344035f75c5027c1834d4aa2b977c77c3876d6e9
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-09-06 22:42:12 +0600

    Create vector_rank_1_tensors_in_numpy.py

---
+++ vector_rank_1_tensors_in_numpy.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  6 22:37:10 2021
+
+@author: Mahfuz_Shazol
+"""
+
+import numpy  as np
+
+
+numpy_array=np.array([15,62,34])
+
+print(type(numpy_array))
+
+print(numpy_array.shape)
+
+
+#get 2nd element
+print(' 2nd element is ',numpy_array[1])
+
+#get type of any element in array
+print('single element:-',type(numpy_array[0]))
+
+
