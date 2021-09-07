51efb5fd9878735b13b71e3bacc1b20d5bc5f386
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-09-08 00:29:01 +0600

    Create l2_norm_with_numpy.py

---
+++ l2_norm_with_numpy.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  8 00:24:44 2021
+
+@author: Mahfuz_Shazol
+"""
+
+import numpy as np
+
+x=np.array([25,2,8])
+
+result=np.linalg.norm(x)
+
+print('Norm Value',result)
