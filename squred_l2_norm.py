ec134fd00f6cdff5cbe85d52054da7395b769b1a
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-09-08 00:43:52 +0600

    Create squred_l2_norm.py

---
+++ squred_l2_norm.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  8 00:40:04 2021
+
+@author: Mahfuz_Shazol
+"""
+import numpy as np 
+
+x=np.array([25,2,5])
+result=np.dot(x,x)
+print('Squred L2 Norm ',result)
+
