aaf0e12eaeedc17a15933d0417a80167d8130afe
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-09-08 00:46:18 +0600

    Create max_norm.py

---
+++ max_norm.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  8 00:44:05 2021
+
+@author: Mahfuz_Shazol
+"""
+
+import numpy as np
+
+x=np.array([25,2,5])
+
+result=np.max([np.abs(25),np.abs(2),np.abs(5)])
+
+print(result)
