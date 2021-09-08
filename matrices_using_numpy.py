79c323d595d609057989c6dd51b6a9685350d65f
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-09-09 01:46:31 +0600

    Create matrices_using_numpy.py

---
+++ matrices_using_numpy.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  9 01:29:59 2021
+
+@author: Mahfuz_Shazol
+"""
+
+import numpy as np
+
+
+X=np.array([ [1,0],
+            [5,4],
+            [8,9],
+            [1,2],
+            ]);
+
+
+print(X.shape)
+
+#get all first row items
+print(X[:,0])
+
+#get 3rd row items
+print(X[2,:])
+
+#slicing by index
+print(X[1:3])
+
+
+
