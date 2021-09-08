c755576033887047907626fbefedcbee101789ad
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-09-09 01:21:54 +0600

    Create orthoginal_vector_using_numpy.py

---
+++ orthoginal_vector_using_numpy.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  9 01:18:11 2021
+
+@author: Mahfuz_Shazol
+"""
+import numpy as np
+
+x=np.array([1,0])
+y=np.array([0,1])
+result=np.dot(x,y)
+
+print(result)
