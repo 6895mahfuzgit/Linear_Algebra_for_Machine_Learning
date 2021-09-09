8c8890cd8b69f3a62787b3f9b609c98145f146ed
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-09-10 02:21:26 +0600

    Create tensor_transposition_using_numpy.py

---
+++ tensor_transposition_using_numpy.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 10 02:07:56 2021
+
+@author: Mahfuz_Shazol
+"""
+
+
+import numpy as np
+
+
+x=np.array([[12,56],
+            [6,44],
+            [1,96],
+            ])
+
+result=x.T
+print(result)
