885f9d81d84406a9bd3c45ac888bcc82d0fd844e
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-09-11 02:41:53 +0600

    Create element_wise_arithmetic_of_2_matrices_using_numpy.py

---
+++ element_wise_arithmetic_of_2_matrices_using_numpy.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 11 02:35:56 2021
+
+@author: Mahfuz_Shazol
+"""
+import numpy as np
+
+
+X=np.array([[12,56],
+            [6,44],
+            [1,96],
+            ])
+
+A=X+2*3
+
+#add
+add_result=X+A
+print('After add ',add_result)
+print()
+print()
+
+#Multi
+multi_result=X+A
+print('after multi ',multi_result)
+print()
+print()
+
+#Div
+div_result=X/A
+print('after div ',div_result)
+print()
+print()
+
+#sub
+sub_result=A-X
+print('after sub ',sub_result)
+print()
+print()
+
+
+
+
