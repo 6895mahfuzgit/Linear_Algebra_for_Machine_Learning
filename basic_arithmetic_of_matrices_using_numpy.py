2a8d931b9d91166e94b886d4f061ae693aa40a4e
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-09-11 02:03:37 +0600

    Create basic_arithmetic_of_matrices_using_numpy.py

---
+++ basic_arithmetic_of_matrices_using_numpy.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 11 01:52:30 2021
+
+@author: Mahfuz_Shazol
+"""
+
+import numpy as np
+
+
+X=np.array([[12,56],
+            [6,44],
+            [1,96],
+            ])
+
+#add
+X_additionX=X+2
+print('After add ',X_additionX)
+print()
+print()
+
+#Multi
+X_multi=X*2
+print('after multi ',X_multi)
+print()
+print()
+
+#Div
+X_div=X/2
+print('after div ',X_div)
+print()
+print()
+
+#sub
+X_sub=X-1
+print('after sub ',X_sub)
+print()
+print()
+
+#multi Operations 
+X_result= X*2+1
+print('result ',X_result)
+print()
+print()
+
