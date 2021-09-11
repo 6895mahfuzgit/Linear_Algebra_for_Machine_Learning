201836eb9f6d00b2cd64af912c448cbf73ca91c6
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-09-12 02:13:48 +0600

    Create dot_product_using_numpy.py

---
+++ dot_product_using_numpy.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 12 02:10:27 2021
+
+@author: Mahfuz_Shazol
+"""
+
+import numpy as np
+
+
+x=np.array([1,2,3])
+y=np.array([7,8,9])
+
+result=np.dot(x,y)
+print(result)
