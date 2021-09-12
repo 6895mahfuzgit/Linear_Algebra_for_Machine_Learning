0e702d93c85d61f5e9de9e876b548be861c7c56c
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-09-13 01:58:41 +0600

    Create matrix_multiplication_with_vestor_using_numpy.py

---
+++ matrix_multiplication_with_vestor_using_numpy.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 13 01:54:27 2021
+
+@author: Mahfuz_Shazol
+"""
+import numpy as np
+
+
+x=np.array([
+          [3,4],
+          [5,6],
+          [7,8]
+         ])
+
+y=np.array([1,2])
+
+
+result=np.dot(x,y)
+print(result)
+
